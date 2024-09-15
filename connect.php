92f8f8d5e379f4b3829f2952825fc2f671179778
sumanth778 <r131948@rguktrkv.ac.in>
2024-09-15 13:03:02 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
 	$host = "dbramsayz.mysql.database.azure.com";
 	$username = "ramsayz@dbramsayz";
 	$password = "Chinnu@778";
@@ -12,8 +12,8 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(id, firstname, lastname) values(?, ?, ?)");
-		$stmt->bind_param("iss",$id, $firstname, $lastname);
+		$stmt = $conn->prepare("insert into registration(id,firstname, lastname) values(?, ?, ?)");
+		$stmt->bind_param("iss", $id, $firstName, $lastName);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successful...";
